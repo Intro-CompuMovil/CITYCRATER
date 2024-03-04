5cff60bd648bbade53527749c2ee2e74afceb762
BoredDany <141600976+BoredDany@users.noreply.github.com>
2024-03-04 13:18:23 -0500

    Add files via upload
    1st GUI

---
+++ cc/app/src/main/java/com/example/citycrater/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.example.citycrater
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val btnSignIn = findViewById<Button>(R.id.btnSign)
+        val btnLogIn = findViewById<Button>(R.id.btnLog)
+
+        btnSignIn.setOnClickListener {
+            val intent = Intent(this, HomeActivity::class.java)
+            startActivity(intent)
+        }
+
+        btnLogIn.setOnClickListener {
+            val intent = Intent(this, HomeActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
