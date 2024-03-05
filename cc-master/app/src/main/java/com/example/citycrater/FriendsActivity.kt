4f534ffb2435d804b01799c15801ded0c5d0a3bf
BoredDany <martinezq.d@javeriana.edu.co>
2024-03-04 23:32:15 -0500

    TRY

--- cc-master/app/src/main/java/com/example/citycrater/FriendsActivity.kt
+++ cc-master/app/src/main/java/com/example/citycrater/FriendsActivity.kt
@@ -7,5 +7,6 @@ class FriendsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_friends)
+        //FRIENDS ACTIVITY (CONDUCTORES)
     }
 }
