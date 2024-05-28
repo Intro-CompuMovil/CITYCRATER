6395a18aff82a074e5ce008f41c2e4ac9f4b134b
Guorti <Gustavo.ortiz7319@gmail.com>
2024-05-28 01:07:53 -0500

    Route without bumps added

--- cc-master/app/src/main/java/com/example/citycrater/MapActivity.kt
+++ cc-master/app/src/main/java/com/example/citycrater/MapActivity.kt
@@ -51,6 +51,8 @@ import java.io.IOException
 import kotlin.random.Random
 import kotlin.math.*
 
+
+
 class MapActivity : AppCompatActivity() {
 
     lateinit var binding: ActivityMapBinding
@@ -162,44 +164,44 @@ class MapActivity : AppCompatActivity() {
 
         childEventListener = object : ChildEventListener {
             override fun onChildAdded(dataSnapshot: DataSnapshot, previousChildName: String?) {
-                    val newBump = dataSnapshot.getValue(Bump::class.java)
-                    if (newBump != null) {
-                        Log.d(
-                            TAG,
-                            "New bump: ${newBump.latitude}, ${newBump.longitude}, ${newBump.size}"
-                        )
-                        // TODO: Update your UI here with the new bump
-                        bumpLocationMarker = createMarkerRetMark(
-                            GeoPoint(newBump.latitude, newBump.longitude),
-                            "Size: ${newBump.size}",
-                            null,
-                            R.drawable.baseline_location_pin_25
-                        )
-                        bumpLocationMarker.let { map!!.overlays.add(it) }
+                val newBump = dataSnapshot.getValue(Bump::class.java)
+                if (newBump != null) {
+                    Log.d(
+                        TAG,
+                        "New bump: ${newBump.latitude}, ${newBump.longitude}, ${newBump.size}"
+                    )
+                    // TODO: Update your UI here with the new bump
+                    bumpLocationMarker = createMarkerRetMark(
+                        GeoPoint(newBump.latitude, newBump.longitude),
+                        "Size: ${newBump.size}",
+                        null,
+                        R.drawable.baseline_location_pin_25
+                    )
+                    bumpLocationMarker.let { map!!.overlays.add(it) }
 
-                        // Create local variables for the latitude, longitude, and key
-                        val latitude = newBump.latitude.toString()
-                        val longitude = newBump.longitude.toString()
-                        val size = newBump.size
-                        val key = dataSnapshot.key
+                    // Create local variables for the latitude, longitude, and key
+                    val latitude = newBump.latitude.toString()
+                    val longitude = newBump.longitude.toString()
+                    val size = newBump.size
+                    val key = dataSnapshot.key
 
-                        bumpLocationMarker?.setOnMarkerClickListener { marker, mapView ->
-                            val intent = Intent(baseContext, ReportFixedActivity::class.java)
-                            intent.putExtra("latitude", latitude)
-                            intent.putExtra("longitude", longitude)
-                            intent.putExtra("size", size)
-                            intent.putExtra("key", key)
-                            startActivity(intent)
-                            true
-                        }
+                    bumpLocationMarker?.setOnMarkerClickListener { marker, mapView ->
+                        val intent = Intent(baseContext, ReportFixedActivity::class.java)
+                        intent.putExtra("latitude", latitude)
+                        intent.putExtra("longitude", longitude)
+                        intent.putExtra("size", size)
+                        intent.putExtra("key", key)
+                        startActivity(intent)
+                        true
+                    }
 
-                        // Store the marker in the HashMap
-                        bumpLocationMarker?.let { marker ->
-                            dataSnapshot.key?.let { key ->
-                                markers[key] = marker
-                            }
+                    // Store the marker in the HashMap
+                    bumpLocationMarker?.let { marker ->
+                        dataSnapshot.key?.let { key ->
+                            markers[key] = marker
                         }
                     }
+                }
             }
 
 
